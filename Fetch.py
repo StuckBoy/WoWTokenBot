a8385f764034b8fb9401510f338335f4f21b6b19
Stuckboy <zstuck96@gmail.com>
2018-08-10 14:05:43 -0400

    Python file can successfully fetch token prices.

---
+++ Fetch.py
@@ -0,0 +1,22 @@
+"File requires requests and termcolor"
+
+import requests, json, re
+from termcolor import colored
+
+token_url = "https://wowtokenprices.com/current_prices.json"
+
+r = json.loads(requests.get(token_url).text)
+
+def checkNumber(number):
+    pattern = re.compile("^-")
+    if pattern.findall(str(number)):
+        return colored(str(number), "red")
+    else:
+        return colored(str(number), "green")
+
+for region in r:
+    current_price = r[region]['current_price']
+    last_change = r[region]['last_change']
+    time_of_last_change = r[region]['time_of_last_change_utc_timezone']
+    change_effect = checkNumber(last_change)
+    print(region + ", " + str(current_price) + ", Change: " + change_effect + " occurred at " + time_of_last_change + " UTC")

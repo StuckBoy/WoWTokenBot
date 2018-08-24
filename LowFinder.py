516bb5521f8063021d76ee52b28f70643c20d2bc
Zachary Stuck <30481102+stuckboy@users.noreply.github.com>
2018-08-24 15:26:21 -0400

    Broke code into readable blocks. Added apscheduler to allow recurring lookups.

--- LowFinder.py
+++ LowFinder.py
@@ -1,28 +1,52 @@
 """Fetches current price, and compares against 30 day price.
-Requires requests and win10Toast for desktop notifications"""
+Requires requests, AP Scheduler, and win10Toast for desktop notifications"""
 
-import requests, json, re
+import requests, json, time, datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
 from win10toast import ToastNotifier
 
-curr_token_url = "https://wowtokenprices.com/current_prices.json"
-month_token_url = "https://wowtokenprices.com/history_prices_30_day.json"
+def fetchJSON():
+    """Function starts by fetching JSON from two URLs, one containing the current in-game gold price for a WoW Token,
+        and the other containing history for the price over the last thirty days. It then scans through the last thirty
+        for the lowest price, and compares it to the current price."""
 
-r1 = json.loads(requests.get(curr_token_url).text)
-r2 = json.loads(requests.get(month_token_url).text)
+    curr_token_url = "https://wowtokenprices.com/current_prices.json"
+    month_token_url = "https://wowtokenprices.com/history_prices_30_day.json"
 
-current_price = r1['us']['current_price']
-last_change = r1['us']['last_change']
-time_of_last_change = r1['us']['time_of_last_change_utc_timezone']
+    r1 = json.loads(requests.get(curr_token_url).text)
+    r2 = json.loads(requests.get(month_token_url).text)
+    findPrice(r1, r2)
 
-lowest_price = 1000000000
 
-for entry in r2["us"]:
-    if entry["price"] < lowest_price:
-        lowest_price = entry["price"]
+def findPrice(r1, r2):
+    """Function scans through provided JSON in search of lowest price in the past 30 days. Upon finding result, the
+        outcome of the comparision determines the response of the script."""
 
-toaster = ToastNotifier()
+    current_price = r1['us']['current_price']
+    last_change = r1['us']['last_change']
+    time_of_last_change = r1['us']['time_of_last_change_utc_timezone']
 
-if lowest_price > current_price:
-    toaster.show_toast("WoW Token Bot", str(current_price) + ", BUY NOW!")
-else:
-    toaster.show_toast("WoW Token Bot", "Lowest: " + str(lowest_price) + " < Current: " + str(current_price))
+    lowest_price = 1000000000
+
+    for entry in r2["us"]:
+        if entry["price"] < lowest_price:
+            lowest_price = entry["price"]
+
+    toaster = ToastNotifier()
+
+    NotifTitle = "WoW Token Bot"
+    curr_string = str(current_price)
+    low_string = str(lowest_price)
+
+    if lowest_price > current_price:
+        toaster.show_toast(NotifTitle, curr_string + ", BUY NOW!", duration=10, threaded=True)
+    else:
+        toaster.show_toast(NotifTitle, "Lowest: " + low_string + " < Current: " + curr_string, duration=1800, threaded=True)
+
+    while toaster.notification_active():
+        time.sleep(0.1)
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(fetchJSON(), 'interval', hours=1)
+scheduler.start()
